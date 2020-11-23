055c6824a36e8c8f4c91c95d3b90d4fbc80eb0d0
Christopher <chrisand.c@gmail.com>
2020-11-22 22:13:32 -0300

    Revert "Revert "version 2""
    This reverts commit dff7b723a70b69b2f03631dc98f3ee01faf6213b.

--- WebApplication4/Controllers/BDDController.cs
+++ WebApplication4/Controllers/BDDController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.SqlClient;
+using WebApplication4.Models.Token;
 
 namespace WebApplication4.Controllers
 {
@@ -12,39 +13,384 @@ namespace WebApplication4.Controllers
         // GET: BDD
         public ActionResult Respuestas(string user, string pass)
         {
+            
+            Token Token = new Token();
+            
+            if (Token.checkUser(user, pass))
+            {
+               string token = Token.createToken(user, pass);
 
+               ViewBag.Token = token;
+               ViewBag.Mensaje = user+pass+token;
 
-            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDDCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+               return View();
+            }
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+        public ActionResult ingresarUsuario(string Usuario, string Clave, string Rut, string Telefono, string Oficina, string Especialidad, int Tipo)
+        {
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             var connection = new SqlConnection(conex);
             connection.Open();
-            string query = "select * from USUARIOS where USUARIO = '"+user+"' AND CLAVE = '"+pass+"'";
-            SqlCommand command;
-            SqlDataReader dataReader;
-            string result = "";
+            string query = "guardarUsuario";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
 
-            command = new SqlCommand(query,connection);
-            dataReader = command.ExecuteReader();
+            command.Parameters.AddWithValue("@Usuario", Usuario);
+            command.Parameters.AddWithValue("@Clave", Clave);
+            command.Parameters.AddWithValue("@Rut", Rut);
+            command.Parameters.AddWithValue("@Telefono", Telefono);
+            command.Parameters.AddWithValue("@Oficina", Oficina);
+            command.Parameters.AddWithValue("@Especialidad", Especialidad);
+            command.Parameters.AddWithValue("@Tipo", Tipo);
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
 
-            while (dataReader.Read())
+
+            int row_count = command.ExecuteNonQuery();
+
+            if (row_count == 0)
+            {
+
+                ViewBag.mensaje = "error al agregar";
+
+            }
+            else if (row_count > 0)
+            {
+                ViewBag.mensaje = "usuario agregado correctamente ";
+            }
+            connection.Close();
+
+
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+        public ActionResult eliminarUsuario(string Rut)
+        {
+
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = new SqlConnection(conex);
+            connection.Open();
+            string query = "eliminarUsuario";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            ;
+            command.Parameters.AddWithValue("@Rut", Rut);
+
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
+
+
+            int row_count = command.ExecuteNonQuery();
+
+            if (row_count == 0)
+            {
+
+                ViewBag.mensaje = "error al eliminar";
+
+            }
+            else if (row_count > 0)
+            {
+                ViewBag.mensaje = "usuario eliminado correctamente ";
+            }
+            connection.Close();
+
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+        public ActionResult editarUsuario(string Usuario, string Clave, string Rut, string Telefono, string Oficina, string Especialidad, int Tipo)
+        {
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = new SqlConnection(conex);
+            connection.Open();
+            string query = "editarUsuario";
+            string query2 = "select * from usuarios where usuario ='" + Usuario + "'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlCommand command2 = new SqlCommand(query2, connection);
+
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@Usuario", Usuario);
+            command.Parameters.AddWithValue("@Clave", Clave);
+            command.Parameters.AddWithValue("@Rut", Rut);
+            command.Parameters.AddWithValue("@Telefono", Telefono);
+            command.Parameters.AddWithValue("@Oficina", Oficina);
+            command.Parameters.AddWithValue("@Especialidad", Especialidad);
+            command.Parameters.AddWithValue("@Tipo", Tipo);
+
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
+
+            try
+            {
+
+                int row_count = command2.ExecuteNonQuery();
+                if (row_count == 0)
                 {
-                result = dataReader.GetString(0);
+                    ViewBag.mensaje = " no existe el usuario " + Usuario + " en los registros.";
+                }
+                else
+                {
+                    command.ExecuteNonQuery();
                 }
 
-            var mensaje = "";
-            if (result==user)
+            }
+            catch
             {
-                mensaje = "existe";
 
             }
-            else 
-            {
-                mensaje = "no existe";
-            }
-            ViewBag.mensaje = mensaje;
+
 
             connection.Close();
 
-            return View();
+
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+        public ActionResult ingresarPaciente(string Rut, string Nombre, string Telefono, string Email, string Sexo, string Direccion, string Prevision)
+        {
+
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = new SqlConnection(conex);
+            connection.Open();
+            string query = "guardarPaciente";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Rut", Rut);
+            command.Parameters.AddWithValue("@Nombre", Nombre);
+            command.Parameters.AddWithValue("@Telefono", Telefono);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@Sexo", Sexo);
+            command.Parameters.AddWithValue("@Direccion", Direccion);
+            command.Parameters.AddWithValue("@Prevision", Prevision);
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
+
+
+            int row_count = command.ExecuteNonQuery();
+
+            if (row_count == 0)
+            {
+
+                ViewBag.mensaje = "error al agregar";
+
+            }
+            else if (row_count > 0)
+            {
+                ViewBag.mensaje = "paciente agregado correctamente ";
+            }
+            connection.Close();
+
+
+
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+
+        public ActionResult eliminarPaciente(string Rut)
+        {
+
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = new SqlConnection(conex);
+            connection.Open();
+            string query = "eliminarPaciente";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            ;
+            command.Parameters.AddWithValue("@Rut", Rut);
+
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
+
+
+            int row_count = command.ExecuteNonQuery();
+
+            if (row_count == 0)
+            {
+
+                ViewBag.mensaje = "error al eliminar";
+
+            }
+            else if (row_count > 0)
+            {
+                ViewBag.mensaje = "usuario eliminado correctamente ";
+            }
+            connection.Close();
+
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+
+        public ActionResult editarPaciente(string Rut, string Nombre, string Telefono, string Email, string Sexo, string Direccion, string Prevision)
+        {
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = new SqlConnection(conex);
+            connection.Open();
+            string query = "editarPaciente";
+            string query2 = "select * from usuarios where usuario ='" + Rut + "'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlCommand command2 = new SqlCommand(query2, connection);
+
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Rut", Rut);
+            command.Parameters.AddWithValue("@Nombre", Nombre);
+            command.Parameters.AddWithValue("@Telefono", Telefono);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@Sexo", Sexo);
+            command.Parameters.AddWithValue("@Direccion", Direccion);
+            command.Parameters.AddWithValue("@Prevision", Prevision);
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
+
+            int row_count = command2.ExecuteNonQuery();
+            if (row_count == 0)
+            {
+                ViewBag.mensaje = " no existe el paciente " + Rut + " en los registros.";
+            }
+            else
+            {
+                command.ExecuteNonQuery();
+            }
+
+
+
+            connection.Close();
+
+
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+        public ActionResult ingresarServicio(string Servicio, int Precio)
+        {
+
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = new SqlConnection(conex);
+            connection.Open();
+            string query = "guardarServicio";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Nombre", Servicio);
+            command.Parameters.AddWithValue("@Precio", Precio);
+
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
+
+
+            int row_count = command.ExecuteNonQuery();
+
+            if (row_count == 0)
+            {
+
+                ViewBag.mensaje = "error al agregar";
+
+            }
+            else if (row_count > 0)
+            {
+                ViewBag.mensaje = "servicio agregado correctamente ";
+            }
+            connection.Close();
+
+
+
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+        public ActionResult eliminarServicio(string Servicio)
+        {
+
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = new SqlConnection(conex);
+            connection.Open();
+            string query = "eliminarServicio";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            ;
+            command.Parameters.AddWithValue("@Nombre", Servicio);
+
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
+
+
+            int row_count = command.ExecuteNonQuery();
+
+            if (row_count == 0)
+            {
+
+                ViewBag.mensaje = "error al eliminar";
+
+            }
+            else if (row_count > 0)
+            {
+                ViewBag.mensaje = "usuario eliminado correctamente ";
+            }
+            connection.Close();
+
+
+            return View("/Views/Home/Login.cshtml");
+        }
+
+
+        public ActionResult editarServicio(string Servicio, int Precio)
+        {
+
+            string conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBBCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = new SqlConnection(conex);
+            connection.Open();
+            string query = "editarServicio";
+            string query2 = "select * from servicios where NOMBRE_SERVICIO ='" + Servicio + "'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlCommand command2 = new SqlCommand(query2, connection);
+
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Nombre", Servicio);
+            command.Parameters.AddWithValue("@Precio", Precio);
+
+            // command.Parameters.AddWithValue("@clave", pass);
+            ViewBag.mensaje = "";
+
+            int row_count = command2.ExecuteNonQuery();
+            if (row_count == 0)
+            {
+                ViewBag.mensaje = " no existe el servicio " + Servicio + " en los registros.";
+            }
+            else
+            {
+                command.ExecuteNonQuery();
+            }
+
+
+
+            connection.Close();
+
+
+
+            return View("/Views/Home/Login.cshtml");
         }
     }
 }
---
+++ WebApplication4/Models/Token/Token.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace WebApplication4.Models.Token
+{
+    public class Token
+
+    {
+        private static String conex = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDDCRONOS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private SqlConnection connection = new SqlConnection(conex);
+        private SqlCommand command;
+        private SqlDataReader dataReader;
+        private static Random random = new Random();
+
+        public int checkToken(String token)
+        {
+            if (!checkTokenNull(token))
+                return 0;
+
+            if (!checkTokenExists(token))
+                return 1;
+
+            if (!checkTokenValid(token))
+                return 2;
+
+            return 3;
+        }
+
+
+        public string createToken(string user, string pass)
+        {
+            string token = random.Next(100000000, 999999999).ToString();
+            string query = "insert into TOKENS (TOKEN, ID_USUARIO) values ('" + token + "','" + getUserId(user, pass).ToString() + "')";
+            connection.Open();
+            command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+            return token;
+        }
+
+        public int getUserId(string user, string pass)
+        {
+            int userid = 0;
+            connection.Open();
+            string query = "select ID_USUARIO from USUARIOS where USUARIO = '" + user + "' AND CLAVE = '" + pass + "'";
+            command = new SqlCommand(query, connection);
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                userid = dataReader.GetInt32(0);
+            }
+            connection.Close();
+
+            return userid;
+        }
+        public bool checkUser(String user, String pass)
+        {
+            connection.Open();
+            string query = "select Count(*) from USUARIOS where USUARIO = '" + user + "' AND CLAVE = '" + pass + "'";
+
+            int result = 0;
+
+            command = new SqlCommand(query, connection);
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                result = dataReader.GetInt32(0);
+            }
+            connection.Close();
+
+            return result == 1 ? true : false;
+
+        }
+
+        private bool checkTokenNull(String token)
+
+        {
+            if (String.IsNullOrEmpty(token))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool checkTokenExists(String token)
+        {
+            connection.Open();
+            string query = "select COUNT(*) from TOKENS where TOKEN = '" + token + "'";
+            command = new SqlCommand(query, connection);
+            dataReader = command.ExecuteReader();
+            string result = "";
+
+            while (dataReader.Read())
+            {
+                result = dataReader.GetInt32(0).ToString();
+            }
+
+
+            connection.Close();
+
+            return int.Parse(result) > 0 ? true : false;
+        }
+
+        public bool checkTokenValid(String token)
+        {
+            DateTime tokentime = getTokenTime(token);
+            DateTime check = DateTime.Now;
+
+            tokentime = tokentime.AddMinutes(5);
+            int compare = DateTime.Compare(tokentime, check);
+
+            return compare == 1 ? true : false;
+        }
+
+        public DateTime getTokenTime(String token)
+        {
+            connection.Open();
+            string query = "select TIME from TOKENS where TOKEN = '" + token + "'";
+            // query = " select InsertarNombre (Carlos, Pinto) "
+
+            DateTime time = DateTime.Now;
+
+            command = new SqlCommand(query, connection);
+            dataReader = command.ExecuteReader();
+
+            dataReader.Read();
+            time = dataReader.GetDateTime(0);
+            connection.Close();
+
+            return time;
+        }
+    }
+
+}
