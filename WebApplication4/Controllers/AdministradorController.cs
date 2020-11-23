66ebde96da1e69c81ddc0abc1b1d2f1fef054dc5
Kevin Letterer <kevin.letterer@gmail.com>
2020-11-23 02:19:52 -0300

    2am

---
+++ WebApplication4/Controllers/AdministradorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        // GET: Administrador
+        public ActionResult Usuarios()
+        {
+            return View("/Views/Administrador/Usuarios.cshtml");
+        }
+        public ActionResult Citas()
+        {
+            return View("/Views/Administrador/Citas.cshtml");
+        }
+        public ActionResult Horas()
+        {
+            return View("/Views/Administrador/Horas.cshtml");
+        }
+        public ActionResult Pacientes()
+        {
+            return View("/Views/Administrador/Pacientes.cshtml");
+        }
+        public ActionResult Servicios()
+        {
+            return View("/Views/Administrador/Servicios.cshtml");
+        }
+        public ActionResult Logout()
+        {
+            ViewBag.Token = "";
+            return PartialView("/Views/Home/Index.cshtml");
+        }
+    }
+
+}
